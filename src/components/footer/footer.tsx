6e272d141fe968e1bbc2a65179677114022a0740
Natan Souza <natan_gabri00@hotmail.com>
2025-08-19 10:56:28 -0300

    Chore: change style guide application

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,26 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Logo } from "../logo";
 
 export const Footer = () => {
   return (
-    <footer className="border-t border-white/10">
+    <footer className=" bg-gray-500">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
-          <Link href="/">
-            <Image src="/logo.svg" alt="Logo" width={116} height={32} />
-          </Link>
+          <Logo />
 
-          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foregrounded">
-            <Link className="hover:text-primary" href="/termos-de-uso">
+          <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-blue-100">
+            <Link className="hover:text-blue-200" href="/termos-de-uso">
               Termos de uso
             </Link>
             <Link
-              className="hover:text-primary"
+              className="hover:text-blue-200"
               href="/politica-de-privacidade"
             >
               Política de privacidade
             </Link>
-            <Link className="hover:text-primary" href="/feedback">
+            <Link className="hover:text-blue-200" href="/feedback">
               Feedback
             </Link>
           </nav>
