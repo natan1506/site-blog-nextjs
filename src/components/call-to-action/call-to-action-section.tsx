6a23c13a71ce030017179cf8296d4699ec86ce37
Natan Souza <natan_gabri00@hotmail.com>
2025-08-20 10:48:01 -0300

    Feature: call to action section

---
+++ src/components/call-to-action/call-to-action-section.tsx
@@ -0,0 +1,35 @@
+import { ArrowRight, Store } from "lucide-react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { PT_Sans_Caption } from "next/font/google";
+
+const ptSansCaption = PT_Sans_Caption({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+export const CallToAction = () => {
+  return (
+    <section className="py-24 bg-gradient-to-b from-cyan-950/50 tp-gray-700 relative">
+      <div className="bg-cyan-300 rounded-full p-4 w-fit absolute -top-5 left-[48%]">
+        <Store className="text-cyan-100" />
+      </div>
+      <div className="container pt-4">
+        <div className="flex flex-col items-center gap-6 text-center ">
+          <h2
+            className={`${ptSansCaption.className} text-gray-100 text-balance text-heading-xl`}
+          >
+            Crie uma loja online e inicie suas vendas ainda hoje
+          </h2>
+
+          <Button variant="primary" asChild className="mt-6">
+            <Link href="/criar-loja">
+              Criar loja grátis
+              <ArrowRight />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
