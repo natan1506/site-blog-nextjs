a07b1717f0ce479facb172fe75d8b1758eddd074
Natan Souza <natan_gabri00@hotmail.com>
2025-08-21 15:23:46 -0300

    Chore: refactor os sections and another adjusts

--- src/templates/sections/call-to-action/call-to-action-section.tsx
+++ src/templates/sections/call-to-action/call-to-action-section.tsx
@@ -1,14 +1,15 @@
 import { ArrowRight, Store } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "../../../components/ui/button";
 import Link from "next/link";
 
 export const CallToAction = () => {
   return (
     <section className="py-24 bg-gradient-to-b from-cyan-950/50 tp-gray-700 relative">
-      <div className="bg-cyan-300 rounded-full p-4 w-fit absolute -top-5 left-[48%]">
+      <div className="bg-cyan-300 rounded-full p-4 w-fit absolute -top-5 left-[48%] z-10">
         <Store className="text-cyan-100" />
       </div>
-      <div className="container pt-4">
+      <div className="absolute inset-0 bg-[url('/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+      <div className="container pt-4 relative">
         <div className="flex flex-col items-center gap-6 text-center ">
           <h2 className="font-sans text-gray-100 text-balance text-heading-xl">
             Crie uma loja online e inicie suas vendas ainda hoje
--- src/templates/sections/hero-section/hero-section.tsx
+++ src/templates/sections/hero-section/hero-section.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
 import { ArrowRight, Clock, Store } from "lucide-react";
-import { Button } from "../ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -15,13 +15,13 @@ export const HeroSection = () => {
             <div>
               <div className="flex items-center gap-2">
                 <Clock className="text-cyan-100 h-4 w-4" />
-                <span className="text-gray-200">
+                <span className="text-body-md text-gray-200">
                   Crie o seu site em menos de 5 minutos
                 </span>
               </div>
               <div className="flex items-center gap-2">
                 <Store className="text-cyan-100 h-4 w-4" />
-                <span className="text-gray-200">
+                <span className="text-body-md text-gray-200">
                   Acompanhe e otimize seu negócio online
                 </span>
               </div>
